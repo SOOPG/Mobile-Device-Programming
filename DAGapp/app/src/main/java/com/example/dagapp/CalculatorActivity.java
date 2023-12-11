cd12351c21dfdbda82f5713c9e86f24281c7af83
SOOPG <wilem.mhsoo@gmail.com>
2023-12-12 06:57:58 +0800

    Functionality: Added Calculator and Ability to Contact Department Heads
    -Added Calculator functionality
    -Added Contact Number functionality

--- DAGapp/app/src/main/java/com/example/dagapp/CalculatorActivity.java
+++ DAGapp/app/src/main/java/com/example/dagapp/CalculatorActivity.java
@@ -1,20 +1,80 @@
 package com.example.dagapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private EditText editTextNumber1;
+    private EditText editTextNumber2;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        editTextNumber1 = findViewById(R.id.inputCalculator);
+        editTextNumber2 = findViewById(R.id.inputCalculator2);
+        textViewResult = findViewById(R.id.outputCalculator);
     }
 
     public void onReturnClicked(View view) {
         finish();
     }
 
+    // Addition
+    public void onAddClicked(View view) {
+        double num1 = Double.parseDouble(editTextNumber1.getText().toString());
+        double num2 = Double.parseDouble(editTextNumber2.getText().toString());
+        double result = num1 + num2;
+        textViewResult.setText(String.valueOf(result));
+    }
+
+    // Subtraction
+    public void onSubtractClicked(View view) {
+        double num1 = Double.parseDouble(editTextNumber1.getText().toString());
+        double num2 = Double.parseDouble(editTextNumber2.getText().toString());
+        double result = num1 - num2;
+        textViewResult.setText(String.valueOf(result));
+    }
+
+    // Multiplication
+    public void onMultiplyClicked(View view) {
+        double num1 = Double.parseDouble(editTextNumber1.getText().toString());
+        double num2 = Double.parseDouble(editTextNumber2.getText().toString());
+        double result = num1 * num2;
+        textViewResult.setText(String.valueOf(result));
+    }
+
+    // Division
+    public void onDivideClicked(View view) {
+        double num1 = Double.parseDouble(editTextNumber1.getText().toString());
+        double num2 = Double.parseDouble(editTextNumber2.getText().toString());
+        // Check for division by zero
+        if (num2 != 0) {
+            double result = num1 / num2;
+            textViewResult.setText(String.valueOf(result));
+        } else {
+            textViewResult.setText("Error");
+        }
+    }
+
+    // Copy Result
+    public void onCopyClicked(View view) {
+        // Copy the result to clipboard
+        android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        android.content.ClipData clip = android.content.ClipData.newPlainText("Copied Data", textViewResult.getText().toString());
+        clipboard.setPrimaryClip(clip);
+    }
+
+    // Clear Inputs and Result
+    public void onClearClicked(View view) {
+        editTextNumber1.setText("");
+        editTextNumber2.setText("");
+        textViewResult.setText("");
+    }
 }
--- DAGapp/app/src/main/java/com/example/dagapp/DepartmentInfoActivity.java
+++ DAGapp/app/src/main/java/com/example/dagapp/DepartmentInfoActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
+import android.widget.TextView;
 
 public class DepartmentInfoActivity extends AppCompatActivity {
 
@@ -11,11 +13,29 @@ public class DepartmentInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_department_info);
+
+        // Set up click listeners for contact information
+        setupContactClickListener(R.id.arts_head_contact);
+        setupContactClickListener(R.id.radio_head_contact);
+        setupContactClickListener(R.id.photo_head_contact);
+        setupContactClickListener(R.id.video_head_contact);
+        setupContactClickListener(R.id.marketing_head_contact);
+    }
+
+    // Navigates to contact app when user clicks on the contact number of respective department heads
+    private void setupContactClickListener(int textViewId) {
+        TextView contactView = findViewById(textViewId);
+        contactView.setOnClickListener(v -> {
+            String phoneNumber = contactView.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(intent);
+        });
     }
 
     // This method is called when the return button is clicked
     public void onReturnClicked(View view) {
-        finish(); // Closes this activity and returns to the previous one
+        finish(); // Closes this page and return to Homepage
     }
 
     //Navigate to edit department page
