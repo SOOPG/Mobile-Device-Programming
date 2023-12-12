11acc547a43a9fbc0cbf1285bc92499728ee8c5c
SOOPG <wilem.mhsoo@gmail.com>
2023-12-13 02:01:38 +0800

    Functionality of Username and Profile Roles
    -added the ability to change user name and profile roles

--- DAGapp/app/src/main/java/com/example/dagapp/HomeActivity.java
+++ DAGapp/app/src/main/java/com/example/dagapp/HomeActivity.java
@@ -1,16 +1,43 @@
 package com.example.dagapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView userNameTextView;
+    private TextView userRoleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Initialize TextViews
+        userNameTextView = findViewById(R.id.user_name);
+        userRoleTextView = findViewById(R.id.user_title);
+        // Load the user settings
+        loadUserSettings();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        // Load the user settings again in case they were changed in SettingsActivity
+        loadUserSettings();
+    }
+
+    private void loadUserSettings() {
+        SharedPreferences preferences = getSharedPreferences("UserSettings", MODE_PRIVATE);
+        String userName = preferences.getString("UserName", "Soo Min Hao"); // Default value if none found
+        String userRole = preferences.getString("UserRole", "Vice Pres."); // Default value if none found
+
+        // Set the user settings to the TextViews
+        userNameTextView.setText(userName);
+        userRoleTextView.setText(userRole);
     }
 
     public void onDepartmentsClicked(View view) {
@@ -42,10 +69,10 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public static final int SETTINGS_REQUEST = 1; // Class constant
     public void onSettingsClicked(View view) {
-        // Navigate to Settings page
         Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, SETTINGS_REQUEST);
     }
 
 }
--- DAGapp/app/src/main/java/com/example/dagapp/SettingsActivity.java
+++ DAGapp/app/src/main/java/com/example/dagapp/SettingsActivity.java
@@ -1,11 +1,16 @@
 package com.example.dagapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private EditText userNameEditText;
+    private EditText userRoleEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +18,29 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
+    public void onSaveUserSettingsClicked(View view) {
+
+        userNameEditText = findViewById(R.id.settingUserName); // Your user name EditText ID
+        userRoleEditText = findViewById(R.id.settingUserRole); // Your user role EditText ID
+
+        String userName = userNameEditText.getText().toString();
+        String userRole = userRoleEditText.getText().toString();
+
+
+        // Save to SharedPreferences
+        SharedPreferences preferences = getSharedPreferences("UserSettings", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("UserName", userName);
+        editor.putString("UserRole", userRole);
+        editor.apply(); // or editor.commit();
+
+        // Optional by provide feedback to the user
+        Toast.makeText(this, "Settings saved", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
     //For return button
     public void onReturnClicked(View view) {
-        finish(); // Closes this activity and returns to the previous one
+        finish();
     }
 }
